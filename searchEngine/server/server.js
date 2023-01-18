51102cc17eeda3ef5c50e652286bceb800ae4173
gowantha <gowantha.18@cse.mrt.ac.lk>
2023-01-18 23:19:24 +0530

    seach engine client added

--- searchEngine/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const client = require('./elasticsearch/client');
-
-const app = express();
-
-const port = 3001;
-
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
